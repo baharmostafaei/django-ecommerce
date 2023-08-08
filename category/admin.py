7f400f8eb1aa65575135f92ecc587a0419a6396b
bahare m <baharemostafaei@gmail.com>
2023-08-08 02:28:16 -0700

    add media

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('category_name',)}
+    list_display = ('category_name', 'slug')
+
+admin.site.register(models.Category, CategoryAdmin)
