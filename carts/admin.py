08351191b66db53760c9128079b3dfe14ad59ba9
bahare m <baharemostafaei@gmail.com>
2023-08-15 04:35:41 -0700

    add variations with every details needed

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'datetime_created')
 
-admin.site.register(models.CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
+
+admin.site.register(models.Cart, CartAdmin)
+
+admin.site.register(models.CartItem, CartItemAdmin)
